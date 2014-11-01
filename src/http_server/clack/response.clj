b2ee88ab490819e6d630016a8741994142a10792
Uku Taht <uku.taht@gmail.com>
2014-11-01 00:56:12 +0000

    images working

--- src/http_server/clack/response.clj
+++ src/http_server/clack/response.clj
@@ -21,8 +21,21 @@
 (defn header-lines [headers]
   (apply str (map header-from (vec headers))))
 
+(defn merge-bytes [b1 b2]
+  (with-open [out (new java.io.ByteArrayOutputStream)]
+    (.write out b1 0 (count b1))
+    (.write out b2 0 (count b2))
+    (.toByteArray out)))
+
+(defn leading-bytes [status headers]
+  (.getBytes (str (status-line status)
+                  (header-lines headers)
+                  line-separator)))
+
+(defn bytes-if-not [body]
+  (if (instance? String body)
+    (byte-array (map byte body))
+    body))
+
 (defn raw-response [[status headers body]]
-  (.getBytes (str (status-line  status)
-       (header-lines headers)
-       line-separator
-       body)))
+  (merge-bytes (leading-bytes status headers) (bytes-if-not body)))
