6b86ca39262c4a32880643daa9bd617076ccd905
Uku Taht <uku.taht@gmail.com>
2014-11-14 01:57:53 +0000

    add smoke test for server

--- spec/http_server/server_spec.clj
+++ spec/http_server/server_spec.clj
@@ -1,7 +1,28 @@
 (ns http-server.server_spec
-  (use [http-server.server]
-       [speclj.core]))
+  (:require [http-server.server :as server]
+            [http_server.clack.core :as clack]
+            [speclj.core :refer :all])
+  (:import (java.io ByteArrayOutputStream StringReader BufferedReader Closeable)))
+
+(def closeable (reify Closeable 
+                 (close [this])))
+
+(defn two-hundred-ok-app [_app _req]
+  [200 {} ""])
+
+(def out (atom (ByteArrayOutputStream. )))
+(def request "GET / HTTP/1.1")
+
+(defn fake-out [_socket]
+  @out)
+
+(defn fake-reader [_socket]
+  (BufferedReader. (StringReader. request)))
 
 (describe "server"
-  (it "does something"
-    (should= 1 1 )))
+  (it "responds to a request"
+    (with-redefs [server/get-output-stream fake-out 
+                  server/get-reader        fake-reader]
+  (clack/use-middleware two-hundred-ok-app)
+  (server/process-request closeable clack/clack)
+  (should= "HTTP/1.1 200 OK\r\n\r\n" (.toString @out)))))
--- src/http_server/server.clj
+++ src/http_server/server.clj
@@ -1,32 +1,35 @@
 (ns http-server.server
-  (use [clojure.java.io :as io]
+  (:use [clojure.java.io :as io]
        [http_server.clack.core :as clack]
        [http_server.clack.logger :as logger]
        [http_server.static.core :as static]
        [http_server.clack.basic_auth :as basic]
        [http_server.cob.core :as cob]) 
-  (import [java.net ServerSocket]
+  (:import [java.net ServerSocket]
           [java.util.concurrent Executors]))
 
-(defn receive [socket]
-  (io/reader socket))
+(defn get-output-stream [socket]
+  (io/output-stream socket))
 
-(defn send-sock [socket msg]
-  (with-open [writer (io/output-stream socket)]
-    (.write writer msg)
-    (.flush writer)))
+(defn get-reader [sock]
+  (io/reader sock))
+
+(defn- send-sock [socket msg]
+  (with-open [out (get-output-stream socket)]
+    (.write out msg)
+    (.flush out)))
 
 (defn process-request [sock handler]
-  (with-open [request (receive sock)]
-    (send-sock sock (handler request))
+  (with-open [readr (get-reader sock)]
+    (send-sock sock (handler readr))
     (.close sock)))
 
-(defn serve [port handler]
+(defn- serve [port handler]
   (let [threads (Executors/newFixedThreadPool 30)]
     (with-open [server-sock (ServerSocket. port)]
       (loop []
         (let [sock (.accept server-sock)]
-          (.execute threads (partial process-request sock handler))
+          (.execute threads #(process-request sock handler))
           (recur))))))
 
 (defn start [port public-dir]
