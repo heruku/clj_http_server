c8628145a1e1599c85bc66f3c377256cc559ec1a
Uku Taht <uku.taht@gmail.com>
2015-02-07 19:47:43 +0000

    Allow users to specify port and public dir via command-line

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/tools.cli "0.3.1"]
                  [com.taoensso/timbre "3.3.1"]]
   :profiles {:dev {:dependencies [[speclj "3.1.0"] ]}}
   :plugins [[speclj "3.1.0"]]
--- src/http_server/core.clj
+++ src/http_server/core.clj
@@ -1,6 +1,14 @@
 (ns http-server.core
-  (use [http-server.server :as server])
+  (use [http-server.server :as server]
+       [clojure.tools.cli :refer [parse-opts]])
   (:gen-class))
 
+
+(def cli-options
+  [["-p" "--port PORT" "Port number" :default 5000 :parse-fn #(Integer/parseInt %)]
+   ["-d" "--public-dir PUBLIC_DIR" "" :default "cob_spec/public"] ])
+
 (defn -main [& args]
-  (server/start 5000 "/Users/ukutaht/Desktop/code/http_server/cob_spec/public"))
+  (let [{:keys [options summary]} (parse-opts args cli-options)]
+    (println options)
+    (server/start (:port options) (:public-dir options))))
